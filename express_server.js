9ca1961d8c6315e3ed3a087bbbc7b25de4996db9
shadeying <shadeying@gmail.com>
2019-05-07 23:20:47 +0000

    Add a POST route to receive the form submission and function to generate a random shortURL

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ var app = express();
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,12 +28,31 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let result = '';
+  const all = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for(var i = 0; i < 6; i++){
+    result += all.charAt(Math.floor(Math.random() * all.length));
+  }
+  return result;
+}
 
