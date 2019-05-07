2c5bc8ef8b4c94effcd0795bb24e133ee918e948
shadeying <shadeying@gmail.com>
2019-05-07 23:29:50 +0000

    shortURL-longURL key-value pair are saved to the urlDatabase when it receives a POST request

--- express_server.js
+++ express_server.js
@@ -38,7 +38,8 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
