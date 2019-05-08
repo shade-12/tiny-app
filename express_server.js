bd90b90c4c8f23ecbd5c744b8005e4ceb0977ab9
shadeying <shadeying@gmail.com>
2019-05-08 19:36:25 +0000

    Add a POST route that updates a URL resource

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  const newLongURL = req.body.newLongURL;
+  urlDatabase[req.params.shortURL] = newLongURL;
+  res.redirect("/urls");
+})
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
