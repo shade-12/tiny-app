56939e8b72178f950dc66eaf88eeca1b08d15001
shadeying <shadeying@gmail.com>
2019-05-09 22:31:44 +0000

    Cleaned up

--- express_server.js
+++ express_server.js
@@ -44,14 +44,6 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlsForUser(req.session.user_id),
